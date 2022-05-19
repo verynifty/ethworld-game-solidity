26d429abc8cdf8d5e8849ef49a393ad7c6f7abd1
grandsmarquis <jdourlens@gmail.com>
2022-05-19 15:34:18 +0900

    hmmm

--- src/Game.sol
+++ src/Game.sol
@@ -5,11 +5,18 @@ import "./IRessource.sol";
 
 contract Game {
 
-
-    mapping(address => IRessource) public ressource;
+    mapping(address => IRessource) public fungible;
 
     function registerRessource(address _ressource) public {
         ressource[_ressource] = IRessource(_ressource);
+    }
+
+    function useRessource(address _ressource, address _user, address _amount) public {
+        require(tx.origin == _user, "No rights to burn ressource of others");
+    }
+
+    function craft(address _ressource, uint256 _quantity) public {
+
     }
 
     function sampleNoise() public pure {
---
+++ src/IRessource.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+
+contract IRessource {
+
+
+
+}
