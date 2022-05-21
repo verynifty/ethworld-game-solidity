41957f88ee08a20ae4224981bce94fe0db7d861b
grandsmarquis <jdourlens@gmail.com>
2022-05-21 16:53:41 +0900

    deploy everything

--- test/sample-test.js
+++ test/sample-test.js
@@ -9,13 +9,35 @@ let R1;
 let R2;
 let R3;
 
-beforeEach(function () {
+let ONE_PER_MINUTE = 16660000000000000
+
+beforeEach(async function () {
   console.log("Deploying everything")
 
-  const GameContract = await ethers.getContractFactory("Game");
-  Game = await GameContract.deploy("Hello, world!");
+  const BaseERC20RessourceContract = await ethers.getContractFactory("BaseERC20Ressource")
+  R1 = await BaseERC20RessourceContract.deploy("Resource1", "R1")
+  R2 = await BaseERC20RessourceContract.deploy("Resource2", "R2")
+  R3 = await BaseERC20RessourceContract.deploy("Resource3", "R3")
 
-  console.log(Game)
+
+  const PlanetContract = await ethers.getContractFactory("Planet");
+  Planet = await PlanetContract.deploy()
+  
+  const GameContract = await ethers.getContractFactory("Game");
+  console.log(Planet.address)
+  Game = await GameContract.deploy(Planet.address);
+
+  const MINTER_ROLE = await Planet.MINTER_ROLE();
+  await R1.grantRole(MINTER_ROLE, Game.address);
+  await R2.grantRole(MINTER_ROLE, Game.address);
+  await R3.grantRole(MINTER_ROLE, Game.address);
+
+  
+  Game.registerRessource(0, R1.address, ONE_PER_MINUTE, 90)
+  Game.registerRessource(1, R1.address, ONE_PER_MINUTE, 90)
+  Game.registerRessource(2, R1.address, ONE_PER_MINUTE, 90)
+
+  console.log(MINTER_ROLE)
 });
 
 
