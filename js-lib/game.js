8c1a076c7608952988a1e4576083db4a994a7aba
grandsmarquis <jdourlens@gmail.com>
2022-05-24 11:07:55 +0900

    begin js lib

---
+++ js-lib/game.js
@@ -0,0 +1,13 @@
+function Game (ethers, addresses) {
+    this.ethers = ethers;
+    this.addresses = addresses
+}
+  
+Game.prototype.newPlayer = async function () {
+    
+}
+
+module.exports = Game;
+
+
+  
---
+++ js-lib/player.js
@@ -0,0 +1,13 @@
+function Player (address) {
+    this.address = address;
+}
+  
+Player.prototype.countPlanets = async function () {
+    
+}
+
+Player.prototype.getPlanets = async function () {
+    
+}
+
+module.exports = Player;
