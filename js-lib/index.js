5d21655b3c149dffe01ce3aab0ce90037d1d8a08
grandsmarquis <jdourlens@gmail.com>
2022-05-27 10:34:56 +0900

    instanciate contract from lib

--- js-lib/index.js
+++ js-lib/index.js
@@ -1,11 +1,25 @@
-function GameLib (ethers, addresses) {
+let GameArtifact = require('../artifacts/contracts/Game.sol/Game.json')
+const ethers = require('ethers')
+const GameABI = GameArtifact.abi;
+
+console.log(GameABI)
+function GameLib (provider, addresses) {
     this.ethers = ethers;
     this.addresses = addresses
     console.log("Game Lib initialized", ethers, addresses)
+    this.GameContract = (new ethers.Contract(this.addresses.game, GameABI)).connect(provider);
+    console.log("get config")
+    this.getGameConfig()
 }
   
 GameLib.prototype.countPlanets = async function () {
     
+}
+
+GameLib.prototype.getGameConfig = async function () {
+    console.log("get game config")
+    let a = await this.GameContract.UNIVERSE()
+    console.log(a)
 }
 
 module.exports = GameLib;
