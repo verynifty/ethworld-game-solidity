f4abfe2bf5b42a7940c743af2dbf6175cd716fe9
grandsmarquis <jdourlens@gmail.com>
2022-05-27 10:39:31 +0900

    instanciate planet

--- js-lib/index.js
+++ js-lib/index.js
@@ -1,6 +1,9 @@
 let GameArtifact = require('../artifacts/contracts/Game.sol/Game.json')
+let PlanetArtifact = require('../artifacts/contracts/Planet.sol/Planet.json')
+
 const ethers = require('ethers')
 const GameABI = GameArtifact.abi;
+const PlanetABI = PlanetArtifact.abi;
 
 console.log(GameABI)
 function GameLib (provider, addresses) {
@@ -8,18 +11,21 @@ function GameLib (provider, addresses) {
     this.addresses = addresses
     console.log("Game Lib initialized", ethers, addresses)
     this.GameContract = (new ethers.Contract(this.addresses.game, GameABI)).connect(provider);
+    this.PlanetContract = (new ethers.Contract(this.addresses.planet, PlanetABI)).connect(provider);
+
     console.log("get config")
     this.getGameConfig()
 }
   
-GameLib.prototype.countPlanets = async function () {
+GameLib.prototype.getPlayerPlanets = async function () {
     
 }
 
 GameLib.prototype.getGameConfig = async function () {
     console.log("get game config")
-    let a = await this.GameContract.UNIVERSE()
-    console.log(a)
+    this.UNIVERSE = (await this.GameContract.UNIVERSE()).toNumber()
+    this.DIFFICULTY = (await this.GameContract.DIFFICULTY()).toNumber()
+    console.log("loaded game params", this.UNIVERSE, this.DIFFICULTY)
 }
 
 module.exports = GameLib;
