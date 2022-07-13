efe54ea49e9e0825829a62ef20120af858afd606
grandsmarquis <jdourlens@gmail.com>
2022-07-13 13:57:28 +0200

    planet names

---
+++ contracts/PlanetNames.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract PlanetNames {
+
+    string[] public first = [
+        "Friso",
+        "Nitedan",
+        "Slandspia",
+        "Frenchle",
+        "Niatar",
+        "Republic of",
+        "Ferael",
+        "Neczechking",
+        "Rymal",
+        "Sobiet",
+        "United States of",
+        "Territory of",
+        "Southern",
+        "Dimensional",
+        "Future of",
+        "The great",
+        "The low",
+        "The bottom",
+        "The top",
+        "Desertic",
+        "Arid",
+        "Acid",
+        "Icy",
+        "Ministry of",
+        "Iced",
+        "Remainings of"
+    ];
+    
+    string[] public second = [
+        "Name",
+        "Kush",
+        "Pol",
+        "Ger",
+        "Fran",
+        "Pleb",
+        "Chad",
+        "Ja",
+        "Costa",
+        "Su",
+        "Zhu",
+        "Gro",
+        "U",
+        "A",
+        "I",
+        "O",
+        "E",
+        "Y",
+        "Bel"
+    ];
+
+    string[] public third = [
+        "cly",
+        "zea",
+        "stan",
+        "bistan",
+        "pro",
+        "ko",
+        "tua",
+        "many",
+        "bo",
+        "thua",
+        "ce",
+        "Gium",
+        "snia",
+        "rusa",
+        "quein",
+        "keeling",
+        "tish",
+        "co",
+        "rica",
+        "mocan",
+        "droon",
+        "bang",
+        "gan",
+        "stinerue",
+        "tesco",
+        "wanme",
+        "dimenbru",
+        "luke",
+        "debe",
+        "dran",
+        "bar",
+        "bo"
+    ]
+    string[] public fourth = [
+        "Kingdom",
+        "Democracy",
+        "DAO",
+        "of the montains",
+        "of the seas",
+        "lands",
+        "peak"
+    ];
+}
